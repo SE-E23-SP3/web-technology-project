c46f2ce20b71687746ecd439fd15022ce8179792
MidnightRocket <git@midnightrocket.dev>
2023-09-27 11:29:01 +0200

    Run composer install before serving

--- develmode.sh
+++ develmode.sh
@@ -9,7 +9,13 @@ if [ ! -e "$DOCKER_BIN" ]; then
 	DOCKER_BIN="$(command -v nerdctl)"
 fi
 
+if [ ! -e ".env" ]; then
+	cp .env.example .env
+	echo "copying to .env" >&2
+fi
 HOST_PORT="8080"
 GUEST_PORT="$HOST_PORT"
 
+
+$DOCKER_BIN run --rm -it --volume "$PWD":/app composer:latest composer install
 $DOCKER_BIN run --rm -it -v "$PWD":/app -p "$HOST_PORT":"$GUEST_PORT" composer:latest php artisan serve --port "$GUEST_PORT"
