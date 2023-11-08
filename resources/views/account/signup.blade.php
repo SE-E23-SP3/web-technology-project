acda1e12f2d89ddda7076e241143be309220b10e
MidnightRocket <git@midnightrocket.dev>
2023-11-08 10:26:21 +0100

    Use component layout in login and signup

--- resources/views/account/signup.blade.php
+++ resources/views/account/signup.blade.php
@@ -1,20 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>SMDB </title>
+<x-layouts.base title="Signup">
+    <x-slot:head>
+        <link rel="stylesheet" href="{{asset('css/login.css')}}">
+    </x-slot:head>
 
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <link rel="stylesheet" href="{{asset('css/login.css')}}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <script defer src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-
-</head>
-<body>
     <div class="container-center">
         <article class="Center">
             <h1>Create new Account</h1>
@@ -27,11 +15,9 @@
                 <label>Password:</label> <br />
                 <input id="password" name="password" type="password"> <br />
                 <label>Confirm password:</label> <br />
-                <input id="password" name="password" type="password"> <br /><br />
+                <input id="password_repeat" name="password_repeat" type="password"> <br /><br />
                 <button type="submit" class="SigninBtn">Create Account</button>
             </form>
         </article>
     </div>
-
-</body>
-</html>
+</x-layouts.base>
