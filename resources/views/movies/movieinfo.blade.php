852fd69020c1136d8d19af2af13d2d2a9fa88e9c
alkor22 <alkor22@student.sdu.dk>
2023-11-22 11:57:45 +0100

    Fixed ccs 2

--- resources/views/movies/movieinfo.blade.php
+++ resources/views/movies/movieinfo.blade.php
@@ -24,6 +24,14 @@
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <link rel="stylesheet" href="{{asset('css/movieinfo.css')}}">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        
+        <style>
+            body{
+    background-color: var(--Background);
+    color: var(--Text);
+    overflow-x: hidden;
+}
+        </style>
     </x-slot:head>
 
     <body>
