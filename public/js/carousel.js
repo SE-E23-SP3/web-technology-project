0a9cf1b730d5831b7ab8575ed33cf739a109de47
Anders Thomsen <anders24.thomsen@gmail.com>
2023-11-22 11:27:45 +0100

    Shorting text
    Added a for loop that shortens down text if its bigger than 200 sign and replaces the rest with ...

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -26,5 +26,32 @@
     createMovie("https://picsum.photos/400/200", "Oppenheimer", "lorem Ipsum")
 
     console.log(movies.length);
-    alert(movies.length);
+    //alert(movies.length);
+
+
+
+
+
+
+
+
+    
+    var movie_info = document.getElementsByClassName("Movie-info")
+
+    // Iterate through each paragraph
+    for (var i = 0; i < movie_info.length; i++) {
+        var movieinfo = movie_info[i];
+        
+        // Get the text content of the paragraph
+        var text = movieinfo.textContent;
+    
+        // Check if the text is longer than 15 characters
+        if (text.length > 200) {
+            // Truncate the text to the first 15 characters
+            var shortText = text.substring(0, 200) + "...";
+    
+            // Set the truncated text back to the paragraph
+            movieinfo.textContent = shortText;
+        }
+    }
 })();
