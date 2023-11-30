55983bd83803ab37cf1a8646a71a73bc5a440ad3
Patrick <paknu22@student.sdu.dk>
2023-11-30 17:31:30 +0100

    Revert "Maybe fix?"
    This reverts commit 0e0e23d0db7107b0dab2f8b708f87b9b7f9c447b.

--- resources/views/user-profile/user-profile.blade.php
+++ resources/views/user-profile/user-profile.blade.php
@@ -4,6 +4,12 @@
     <!--Font awesome icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script defer src="https://kit.fontawesome.com/c52cf1851a.js" crossorigin="anonymous"></script>
+<x-layouts.base>
+  <x-slot:head>
+    <link rel="stylesheet" href="{{asset('css/user-profile.css')}}">
+    <!--Font awesome icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script defer src="https://kit.fontawesome.com/c52cf1851a.js" crossorigin="anonymous"></script>
   </x-slot:head>
 
   <body>
--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::get('/hello', function () {
 });
 
 Route::get('/watchlist', function () {
-    return view('/user-profile-page/your-watchlist');
+    return view('/user-profile/user-profile');
 })-> name("watchlist");
 
 
