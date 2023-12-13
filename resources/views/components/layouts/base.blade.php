d3764de60637fdf7c7abfed1d92c04822470ccc7
ANders Uttenthal Thomsen <andth2@student.sdu.dk>
2023-12-13 12:39:52 +0100

    duplicate

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -1,6 +1,5 @@
 <x-layouts.minimum {{ $attributes }}>
     <x-slot:head>
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <script defer src="{{ asset('js/utils.js') }}" type="text/javascript"></script>
         <script defer src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
