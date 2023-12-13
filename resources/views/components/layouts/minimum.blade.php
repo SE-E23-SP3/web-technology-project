5311a0600b8a3b041d7204c00d9cf56322eec641
FearfulSnow <11558593+FearfulSnow@users.noreply.github.com>
2023-12-13 10:48:50 +0100

    moved stylesheet after bootstrap

--- resources/views/components/layouts/minimum.blade.php
+++ resources/views/components/layouts/minimum.blade.php
@@ -5,9 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>{{$attributes->has('title') ? "SMDB - " . $attributes->get('title') : 'SMDB'}}</title>
         <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}" />
-        <link rel="stylesheet" type="text/css" href="{{asset('css/cookie.css')}}" />
         <meta name="csrf-token" content="{{ csrf_token() }}">
         {{ $head ?? null }}
+        <link rel="stylesheet" type="text/css" href="{{asset('css/cookie.css')}}" />
     </head>
     <body>
         {{ $slot }}
