523734cbe538d55bf9f0414ade8187218f58ffa4
FearfulSnow <11558593+FearfulSnow@users.noreply.github.com>
2023-12-13 10:06:47 +0100

    mini save, having problems naming css classes

--- resources/views/components/cookie-consent/cookie.blade.php
+++ resources/views/components/cookie-consent/cookie.blade.php
@@ -1,3 +1,24 @@
-<div class="cookie-container">
-    <h1>Cookie consent</h1>
+<div class="cookie-backgrounds">
+    <div class="cookie-contain">
+        <h1>We value your privacy</h1>
+        <p>we use cookies to enhance your browsing experiance.</p>
+        <button type="button" id="customize-cookies-button">Customize Cookies</button>
+        <button type="button" id="essential-cookies-button">Accept Only Essential Cookies</button>
+        <button type="button" id="all-cookies-button">Accept All Cookies</button>
+    </div>
+    <div class="cookie-contain">
+        <h1>Customize cookies</h1>
+        <p><- Back</p>
+        <div>
+            <label for="">Essential cookies</label>
+            <p>always active</p>
+        </div>
+        <div>
+            <label for="">Analytics cookies</label>
+            <input type="checkbox" name="" id="">
+        </div>
+        <button type="button" id="customize-cookies-button">Customize Cookies</button>
+        <button type="button" id="essential-cookies-button">Accept Only Essential Cookies</button>
+        <button type="button" id="all-cookies-button">Accept All Cookies</button>
+    </div>
 </div>
