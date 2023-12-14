e877b462fecd8b7f454a1d5014eff41d2e00bec0
Patrick <paknu22@student.sdu.dk>
2023-12-14 15:14:12 +0100

    Small fixes
    - Added log out button
    - fixed overlay issue
    - added :active effects
    - Changed font-size and -weight

--- resources/views/account/user-edit.blade.php
+++ resources/views/account/user-edit.blade.php
@@ -1,32 +1,34 @@
-<x-layouts.login-base title="Edit User">
+<x-layouts.base title="Edit User">
 	<x-slot:head>
 		<link rel="stylesheet" href="{{ asset('css/user-edit.css') }}">
 	</x-slot:head>
-	
+	<div class="background">
 	<section class="content">
-		<h1>Edit Profile</h1>
+	<h1>Edit Profile</h1>
 		<article class="change-username">
 			<h2>Change Username</h2>
 			<section class="username-section">
 				<label for="username" id="username">Username:</label>
-				<input type="text" value="Current Username">
+				<input type="text" value="Current Username" id="usrInput">
 			</section>
-			<button class="change-btns" id="saveUsrBtn">Save Username</button>
+			<button class="change-btn-usr" id="saveUsrBtn">Save Username</button>
 		</article>
 		<article class="change-password">
 			<h2>Change Password</h2>
 				<section class="password-inputfields">
-					<label for="current-password" id="currenPassword">Current Password</label>
-					<input type="text" placeholder="password">
+					<label for="current-password" id="currenPassword">Current Password:</label>
+					<input type="text" id="cPwdInput">
 
-					<label for="new-password" id="newPassword">New Password</label>
-					<input type="text">
+					<label for="new-password" id="newPassword">New Password:</label>
+					<input type="text" id="nPwdInput">
 
-					<label for="repeat-password" id="repeatPassword">Repeat Password</label>
-					<input type="text">
+					<label for="repeat-password" id="repeatPassword">Repeat Password:</label>
+					<input type="text" id="rPwdInput">
 				</section>
-				<button class="change-btns" id="savePwBtn">Save password</button>
+				<button class="change-btn-pwd" id="savePwBtn">Save Password</button>
 		</article>
-		<button class="delete-btn" id="deleteBtn">Delete user</button>
+		<button class="log-out-btn" id="logOutBtn">Log out</button>
+		<button class="delete-btn" id="deleteBtn">Delete Account</button>
 	</section>
-</x-layouts.login-base>
+	</div>
+</x-layouts.base>
