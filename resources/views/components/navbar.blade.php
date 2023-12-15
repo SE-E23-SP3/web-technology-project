ad39bcec9754f103d2db2ab27ad5437f5e2abbc2
MidnightRocket <git@midnightrocket.dev>
2023-12-15 04:35:40 +0100

    Add dynamic navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,22 +7,14 @@
         </button>
         <div class="collapse navbar-collapse navbar-colors" id="navbarSupportedContent">
             <ul class="navbar-nav mb-2 mb-lg-0">
-                <li class="">
-                    <a class="" href="{{route('welcome')}}">Home</a>
-                </li>
-                <li class="">
-                    <a class="" href="">watchlist</a>
-                </li>
-                <li>
-                    <a class="" href="{{route('login')}}">Login</a>
-                </li>
-                <li>
-                    <a class="" href="{{route('signup')}}">Signup</a>
-                </li>
+                @foreach ($navbarElements as $key => $url)
+                    <li class="">
+                        <a class="{{url()->current() == URL::to($url) ? 'active-site' : ''}}" href="{{$url}}">{{$key}}</a>
+                    </li>
+                @endforeach
             </ul>
-            <form class="d-flex">
-                @csrf
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <form class="d-flex" action="" method="GET">
+                <input class="form-control me-2" name="q" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success" type="submit">
                     Search
                 </button>
