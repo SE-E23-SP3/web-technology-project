e816081b83d3ea715a0ae788ae18230b0d9596fb
alkor22 <alkor22@student.sdu.dk>
2023-12-20 14:06:15 +0100

    Fixed  requested changes

--- app/Http/Controllers/RateMovieController.php
+++ app/Http/Controllers/RateMovieController.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Http\Response;
 
 class RateMovieController extends Controller
 {
@@ -20,8 +21,8 @@ public function rate(Request $request, Movie $movie)
         $movie->addRating($user, $rating);
 
         return back()->with('message', 'Movie rated!');
-    } else {
-        return redirect()->back()->with('error', 'User not found!');
+    } else{
+        abort(Response::HTTP_NOT_FOUND, 'User not found');
     }
 }
 }
--- app/Http/Controllers/addToWatchlistController.php
+++ app/Http/Controllers/addToWatchlistController.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Http\Response;
 
 class addToWatchlistController extends Controller
 {
@@ -17,8 +18,8 @@ public function addMovieToWatchlist(Request $request, Movie $movie) {
     if ($user != NULL) {
         $user->addMovieToWatchlist($movie);
         return redirect()->back()->with('message', 'Movie added to watchlist!');
-    } else {
-        return redirect()->back()->with('error', 'User not found!');
+    } else{
+        abort(Response::HTTP_NOT_FOUND, 'User not found');
     }
 }
 }
